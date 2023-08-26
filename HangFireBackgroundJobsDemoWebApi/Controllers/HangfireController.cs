41cfd3bc9c62162936f09c4a026e64c9edc9765f
hema325 <hema24790@gmail.com>
2023-08-26 18:35:45 +0300

    Initial

---
+++ HangFireBackgroundJobsDemoWebApi/Controllers/HangfireController.cs
@@ -0,0 +1,65 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireBackgroundJobsDemoWebApi.Controllers
+{
+    [ApiController]
+    [Route("hangfire")]
+    public class HangfireController : ControllerBase
+    {
+        private readonly ILogger<HangfireController> _logger;
+
+        public HangfireController(ILogger<HangfireController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("enqueue")]
+        public IActionResult Enqueue()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendMessage());
+            return Ok(jobId);
+        }
+
+        [HttpGet("requeue")]
+        public IActionResult Requeue(string jobId)
+        {
+            var isRequeued = BackgroundJob.Requeue(jobId);
+            return Ok(isRequeued);
+        }
+
+        [HttpGet("schedule")]
+        public IActionResult Schedule()
+        {
+            var jobId = BackgroundJob.Schedule(() => SendMessage(), TimeSpan.FromMinutes(1));
+            return Ok(jobId);
+        }
+
+        [HttpGet("continueJobWith")]
+        public IActionResult ContinueJobWith(string parentId)
+        {
+            var isRequeued = BackgroundJob.ContinueJobWith(parentId, () => SendMessage());
+            return Ok(isRequeued);
+        }
+
+        [HttpGet("addOrUpdate")]
+        public IActionResult AddOrUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => SendMessage(), Cron.Minutely);
+            return NoContent();
+        }
+
+        [HttpDelete("{jobId}")]
+        public IActionResult DeleteJob(string jobId)
+        {
+            var isDeleted = BackgroundJob.Delete(jobId);
+            return Ok(isDeleted);
+        }
+
+        [NonAction]
+        public void SendMessage()
+        {
+            _logger.LogInformation("Message sent at {dateTime}", DateTime.UtcNow);
+        }
+    }
+}
---
+++ HangFireBackgroundJobsDemoWebApi/Program.cs
@@ -0,0 +1,32 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHangfire(options => options.UseSqlServerStorage(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseHangfireDashboard("/hangfire/dashboard");
+
+app.MapControllers();
+
+RecurringJob.AddOrUpdate("xx", () => Console.WriteLine("main recurring job"), Cron.Minutely);
+
+app.Run();
